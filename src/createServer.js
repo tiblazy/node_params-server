1dd0c2a9a8d451c772e7ddd86f75e51d6c6c4c76
tiblazy <hauntedapply@gmail.com>
2025-03-07 20:57:56 -0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,24 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    const urlParams = {
+      parts: url.pathname
+        ? url.pathname.split('/').filter((pathname) => pathname !== '')
+        : [],
+      query: url.searchParams
+        ? Object.fromEntries(url.searchParams.entries())
+        : {},
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(urlParams));
+  });
 }
 
 module.exports = {
